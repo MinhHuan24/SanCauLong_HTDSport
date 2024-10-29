3661931d20a23b7903370872176d7bdb73361c39
MinhHuan24 <huannguyen.131204@gmail.com>
2024-10-29 15:48:22 +0700

    Cập nhật chiều t3

---
+++ SanCauLong/Controllers/SanController.cs
@@ -0,0 +1,24 @@
+﻿using SanCauLong.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SanCauLong.Controllers
+{
+    public class SanController : Controller
+    {
+        // GET: San
+        SanCauLongEntities db = new SanCauLongEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DanhSachSan()
+        {
+            var sanCauLongs = db.SanCauLongs.ToList();
+            return View(sanCauLongs);
+        }
+    }
+}
